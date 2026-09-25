feb12c0d59618c2f91f58f2e2706ee790f49d8b2
sulabhraj1403 <156576265+sulabhraj1403@users.noreply.github.com>
2026-09-25 23:10:30 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyDxKMF-UXLDli3CeVUEdcg8EmUtrgky8Vk",
-  authDomain: "major-hospital-reception.firebaseapp.com",
-  projectId: "major-hospital-reception",
-  storageBucket: "major-hospital-reception.firebasestorage.app",
-  messagingSenderId: "786849501755",
-  appId: "1:786849501755:web:ed5954dab6e3c594a1421e"
-};
